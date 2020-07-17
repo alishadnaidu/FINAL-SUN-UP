981467deb7c494b32330a9d83460b056f7825f65
alisha naidu <alishadnaidu@gmail.com>
2020-07-16 21:56:09 -0700

    quiz page 6 button colors

--- FINAL-SUN-UP/QuizPage6ViewController.swift
+++ FINAL-SUN-UP/QuizPage6ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class QuizPage6ViewController: UIViewController {
 
+    @IBOutlet weak var under10ButtonOutlet: UIButton!
+    @IBOutlet weak var tenTo15ButtonOutlet: UIButton!
+    @IBOutlet weak var fifteenTo20ButtonOutlet: UIButton!
+    @IBOutlet weak var TwentyAndAboveButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,17 +23,33 @@ class QuizPage6ViewController: UIViewController {
     
     @IBAction func tenAndUnder(_ sender: UIButton) {
         myBigClass.price = "$10 and under"
+        self.under10ButtonOutlet.backgroundColor = UIColor(red: 1.0, green: 0.89, blue: 0.556, alpha: 1.0)
+        self.tenTo15ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.fifteenTo20ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.TwentyAndAboveButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
     }
     
     @IBAction func tenToFifteen(_ sender: UIButton) {
         myBigClass.price = "$10 to $15"
+        self.under10ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.tenTo15ButtonOutlet.backgroundColor = UIColor(red: 1.0, green: 0.89, blue: 0.556, alpha: 1.0)
+        self.fifteenTo20ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.TwentyAndAboveButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
     }
     @IBAction func fifteenToTwenty(_ sender: UIButton) {
         myBigClass.price = "$15 to $20"
+        self.under10ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.tenTo15ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.fifteenTo20ButtonOutlet.backgroundColor = UIColor(red: 1.0, green: 0.89, blue: 0.556, alpha: 1.0)
+        self.TwentyAndAboveButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
     }
     
     @IBAction func twentyToThirtyFive(_ sender: UIButton) {
         myBigClass.price = "$20 to $35"
+        self.under10ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.tenTo15ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.fifteenTo20ButtonOutlet.backgroundColor = UIColor(red: 0.827, green: 0.9176, blue: 1.0, alpha: 1.0)
+        self.TwentyAndAboveButtonOutlet.backgroundColor = UIColor(red: 1.0, green: 0.89, blue: 0.556, alpha: 1.0)
     }
     /*
      // MARK: - Navigation
